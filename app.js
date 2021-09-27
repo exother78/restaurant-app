c71b1455b2bf6840b072c49aa1600dc4e7094f8a
asimimamabdullah <asimimamabdullah@gmail.com>
2021-09-27 19:13:23 +0500

    responsive

--- app.js
+++ app.js
@@ -42,5 +42,4 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server is running on ${port}`));
-
-// mongo_uri=mongodb+srv://asimimam:marvelnet@practice-cluster.ysjju.mongodb.net/restaurant-app?retryWrites=true&w=majority
+// mongo_uri=mongodb://localhost:27017/restaurant-app
